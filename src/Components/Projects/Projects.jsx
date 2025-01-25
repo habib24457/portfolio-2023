5a2e0e84c2e82539f3ffbc8a13d043779497e4b4
habib24457 <habiburehman390@gmail.com>
2025-01-25 12:53:25 +0100

    Added new dotnet project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -8,12 +8,16 @@ import {
   SiNodedotjs,
   SiBootstrap,
   SiJavascript,
+  SiDotnet,
+  SiCsharp,
+  SiMysql,
 } from "react-icons/si";
 import prismImg from "../../assets/projectImages/prism.png";
 import cryptoImg from "../../assets/projectImages/crypto.png";
 import picnicImg from "../../assets/projectImages/picnic.png";
 import cityImg from "../../assets/projectImages/city.png";
 import birdImg from "../../assets/projectImages/bird.png";
+import apiImg from "../../assets/projectImages/endpoints.png";
 
 export const Projects = () => {
   const [pageLanguage, setPageLanguage] = useContext(LanguageContext);
@@ -21,6 +25,21 @@ export const Projects = () => {
   const myProjects = [
     {
       id: 1,
+      projectName: "NZWalk .Net Web API",
+      liveSiteLink: "https://github.com/habib24457/NZWalks.Updated",
+      frontendGitLink:
+        "https://github.com/habib24457/NZWalks.Updated/tree/main/test",
+      backendGitLink: "https://github.com/habib24457/NZWalks.Updated",
+      projectImg: apiImg,
+      projectDescriptionEn:
+        "New Zone Walk is an .Net Core Web API that I have been developing to build a web platform for walk enthusiast. End user can suggest a new walking region and set the difficulty, for other users to follow. ",
+      projectDescriptionDe:
+        "New Zone Walk ist eine .Net Core-Web-API, die ich entwickelt habe, um eine Webplattform für Wanderbegeisterte aufzubauen. Der Endbenutzer kann eine neue Wanderregion vorschlagen und den Schwierigkeitsgrad festlegen, damit andere Benutzer ihm folgen können.",
+      techStack: ".Net 7, EF Core, MS SQL Database",
+      hasFrontend: false,
+    },
+    {
+      id: 2,
       projectName: "React, Node, Mongo Project:Prism Photography",
       liveSiteLink: "https://prism-photo-studio.web.app/",
       frontendGitLink: "https://github.com/habib24457/Prism-photography-client",
@@ -31,9 +50,10 @@ export const Projects = () => {
       projectDescriptionDe:
         "Eine Full-Stack-Website mit Authentifizierungs- (Firebase) und Zahlungs- (Stripe) Gateway. React and Node wird im Frontend und Backend und MongoDB als Datenbank verwendet",
       techStack: "mern",
+      hasFrontend: true,
     },
     {
-      id: 2,
+      id: 3,
       projectName: "React, Typescrip Project: Awesome Crypto",
       liveSiteLink: "https://funny-pavlova-88e535.netlify.app/",
       frontendGitLink:
@@ -45,9 +65,10 @@ export const Projects = () => {
       projectDescriptionDe:
         "Eine React & Typescript-App. Kryptoinformationen von REST API anzeigen.",
       techStack: "mern",
+      hasFrontend: true,
     },
     {
-      id: 3,
+      id: 4,
       projectName: "React, Node, Mongo Project: Picnic Super Market",
       liveSiteLink: "https://picnic-supermarket-auth.web.app/home",
       frontendGitLink: "https://github.com/habib24457/Picnic-e-commerce",
@@ -58,10 +79,11 @@ export const Projects = () => {
       projectDescriptionDe:
         "Eine Full-Stack-E-Commerce-Website mit Authentifizierungs- (Firebase) und Zahlungs- (Stripe) Gateway. React and Node wird im Frontend und Backend und MongoDB als Datenbank verwendet.",
       techStack: "mern",
+      hasFrontend: true,
     },
 
     {
-      id: 4,
+      id: 5,
       projectName: "Javascript project: Handling REST API",
       liveSiteLink: "https://habib24457.github.io/my-fency-slider/",
       frontendGitLink: "https://github.com/habib24457/my-fency-slider",
@@ -71,7 +93,8 @@ export const Projects = () => {
         "A simple Javascript project that gives you high quality pictures.",
       projectDescriptionDe:
         "Ein einfaches Javascript-Projekt, das Ihnen qualitativ hochwertige Bilder liefert.",
-      techStack: "normal",
+      techStack: "mern",
+      hasFrontend: true,
     },
   ];
 
@@ -98,13 +121,19 @@ export const Projects = () => {
                       ? project.projectDescriptionEn
                       : project.projectDescriptionDe}
                   </p>
-                  {project.techStack == "mern" && (
+                  {project.techStack == "mern" ? (
                     <div style={{ margin: "20px" }}>
                       <FaReact />
                       <SiNodedotjs />
                       <SiExpress />
                       <SiMongodb />
                       <SiBootstrap />
+                    </div>
+                  ) : (
+                    <div style={{ margin: "20px" }}>
+                      <SiDotnet />
+                      <SiCsharp />
+                      <SiMysql />
                     </div>
                   )}
 
@@ -120,18 +149,30 @@ export const Projects = () => {
                       target="_blank"
                       rel="noreferrer"
                     >
-                      <span type="button" className="btns onbt onbt">
-                        See this Live
-                      </span>
+                      {project.hasFrontend ? (
+                        <span type="button" className="btns onbt onbt">
+                          See live site
+                        </span>
+                      ) : (
+                        <span type="button" className="btns onbt onbt">
+                          Check code
+                        </span>
+                      )}
                     </a>
                     <a
                       href={project.frontendGitLink}
                       target="_blank"
                       rel="noreferrer"
                     >
-                      <span type="button" className="btns onbt">
-                        View Code
-                      </span>
+                      {project.hasFrontend ? (
+                        <span type="button" className="btns onbt onbt">
+                          View Code
+                        </span>
+                      ) : (
+                        <span type="button" className="btns onbt onbt">
+                          Unit tests
+                        </span>
+                      )}
                     </a>
                   </div>
                 </div>
